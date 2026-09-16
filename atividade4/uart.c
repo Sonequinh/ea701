f317d46a38a7e879138715120e5378bb94c913a7
estrela-16 <mcmo@protonmail.com>
2026-09-16 11:20:24 -0300

    nova pasta

---
+++ atividade4/uart.c
@@ -0,0 +1,11 @@
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+int main(){
+
+    char msg_TXC0 [] = "Transmissao serial utilizando a interrupcao USART Transmit Complete. \n\n";
+
+    return 0;
+}
